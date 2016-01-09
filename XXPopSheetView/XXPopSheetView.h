eb9fefdf5fbc8a3f147ae599b2059c8d26fc4b58
邝清旭 <375961900@qq.com>
2016-01-09 17:36:33 +0800

    更新代理方法、readMe.md 文件

--- XXPopSheetView/XXPopSheetView.h
+++ XXPopSheetView/XXPopSheetView.h
@@ -8,6 +8,13 @@
 
 #import <UIKit/UIKit.h>
 
+@class XXPopSheetView;
+@protocol XXPopSheetDelegate <NSObject>
+
+@optional
+- (void)view:(XXPopSheetView *)popView didSelectAtIndexPath:(NSInteger)index;
+
+@end
 
 @interface XXPopSheetView : UIView
 // 标题
@@ -16,7 +23,7 @@
 @property (nonatomic, copy)NSArray *images;
 // 跳转控制器
 @property (nonatomic, strong)NSArray *viewControllers;
-
+@property (nonatomic, assign)id<XXPopSheetDelegate> delegate;
 
 // 设置宽高
 - (void)setPopViewWidth:(CGFloat)width andHeight:(CGFloat)height;
